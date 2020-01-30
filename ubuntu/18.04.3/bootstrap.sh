806500b828c781284b257d69c7fc09833a10a44b
Patrick Lubach <p.lubach@reply.de>
2020-01-30 21:27:24 +0100

    init commit

---
+++ ubuntu/18.04.3/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y python3 python3-pip git sudo
+
+useradd --create-home --groups sudo --shell /bin/bash ansible
+
+cp -r /home/vagrant/.ssh/ /home/ansible/
+
+chown -R ansible /home/ansible/.ssh/
