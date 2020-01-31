e9373b8d22050551f1268a62f387c42c03fadf65
Patrick Lubach <p.lubach@reply.de>
2020-01-31 11:36:12 +0100

    init commit

---
+++ centOS/7/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Update and install basic tools
+yum update -y
+yum install -y curl \
+               git \
+               python3 \
+               python3-pip \
+               sudo \
+               wget
+
+# Install virtualbox guest additions
+wget http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso
+mkdir /media/VBoxGuestAdditions
+mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
+sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_4.3.8.iso
+umount /media/VBoxGuestAdditions
+rmdir /media/VBoxGuestAdditions
+
+# create a user for ansible
+useradd --create-home --groups vagrant --shell /bin/bash --user-group ansible
+cp -r /home/vagrant/.ssh/ /home/ansible/
+chown -R ansible /home/ansible/.ssh/
