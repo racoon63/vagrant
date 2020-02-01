4f319d0b406b9a206a3cb1eca8ecea46bfd64068
Patrick Lubach <p.lubach@reply.de>
2020-02-01 22:47:45 +0100

    formatted packages for better overview

--- ubuntu/18.04.3/bootstrap.sh
+++ ubuntu/18.04.3/bootstrap.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y python3 python3-pip git sudo
+apt-get install -y curl \
+                   python3 \ 
+                   python3-pip \
+                   git \
+                   wget
 
 useradd --create-home --groups sudo --shell /bin/bash ansible
 
