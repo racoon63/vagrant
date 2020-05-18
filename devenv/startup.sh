107081b6f7d1f71248ae90a7cba0e5b881c20bf3
racoon63 <racoon63@gmx.net>
2020-05-18 21:34:35 +0200

    renamed init by startup

--- devenv/startup.sh
+++ devenv/startup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-echo -e "\033[1;33mCreating dev-net network...\033[0m"
+set -e
+
+echo -e "\033[1;33m===> Creating dev-net network...\033[0m"
 if [[ $(VBoxManage list natnets | grep "dev-net") ]]
 then 
-    echo -e "\033[1;31mNetwork already exists\033[0m"
+    echo -e "\033[1;31m===> Network already exists\033[0m"
 else
     VBoxManage natnetwork add --netname dev-net \
                               --network 10.10.10.0/24 \
@@ -12,8 +14,8 @@ else
                               --ipv6 off
 fi
 
-echo -e "\033[1;33mBringup nodes...\033[0m"
+echo -e "\033[1;33m===> Bringup nodes...\033[0m"
 vagrant up
 
-echo -e "\033[1;33mLogin to first node...\033[0m"
+echo -e "\033[1;33m===> Login to node1...\033[0m"
 vagrant ssh node1
