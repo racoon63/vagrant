221c28e2b8f552af2438ebc7e5d3b5894b6e6a76
racoon63 <racoon63@gmx.net>
2020-05-18 21:34:06 +0200

    added hosts names to /etc/hosts

--- devenv/bootstrap.sh
+++ devenv/bootstrap.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-# TODO
+echo "192.168.5.10 node1" >> /etc/hosts
+echo "10.10.10.10 node1" >> /etc/hosts
+
+echo "192.168.5.20 node2" >> /etc/hosts
+echo "10.10.10.20 node2" >> /etc/hosts
