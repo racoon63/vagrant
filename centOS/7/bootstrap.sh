6e5396c728160b07067033a8e1aaf366cc25f96b
racoon63 <racoon63@gmx.net>
2020-05-18 21:33:11 +0200

    add docker and moved ansible

--- centOS/7/bootstrap.sh
+++ centOS/7/bootstrap.sh
@@ -13,9 +13,17 @@ yum install -y docker-ce \
                docker-ce-cli \
                containerd.io
 
+# ===== Setup docker service on system startup =====
+systemctl enable docker
+usermod -aG docker vagrant
+
+# ===== Create a user for ansible =====
+useradd --create-home --groups vagrant --shell /bin/bash --user-group ansible
+cp -r /home/vagrant/.ssh/ /home/ansible/
+chown -R ansible /home/ansible/.ssh/
+
 # ===== Install custom virtualbox guest additions version =====
 # VBoxGuestAdditions_version="6.0.20" # from: https://download.virtualbox.org/virtualbox/
-
 # wget http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso
 # mkdir /media/VBoxGuestAdditions
 # mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
@@ -23,8 +31,3 @@ yum install -y docker-ce \
 # rm VBoxGuestAdditions_4.3.8.iso
 # umount /media/VBoxGuestAdditions
 # rmdir /media/VBoxGuestAdditions
-
-# ===== Create a user for ansible =====
-useradd --create-home --groups vagrant --shell /bin/bash --user-group ansible
-cp -r /home/vagrant/.ssh/ /home/ansible/
-chown -R ansible /home/ansible/.ssh/
