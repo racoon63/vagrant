c7380f521d8f2e08cef0b154d3dce5c9795e3aee
racoon63 <racoon63@gmx.net>
2020-05-23 00:56:58 +0200

    cleanup

--- devenv/bootstrap.sh
+++ devenv/bootstrap.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-echo "192.168.5.10 node1" >> /etc/hosts
 echo "10.10.10.10 node1" >> /etc/hosts
-
-echo "192.168.5.20 node2" >> /etc/hosts
 echo "10.10.10.20 node2" >> /etc/hosts
